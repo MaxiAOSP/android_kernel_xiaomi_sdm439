6c499cf40db6edfb7d3ad5449f05fb85d2673af8
NV-Dev <alfredomartinez_1423@hotmail.com>
2021-10-06 01:21:44 +0000

    Add DroneCI config

---
+++ .drone.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Setting up build environment...
+apt-get install unzip p7zip-full curl python2 binutils-aarch64-linux-gnu wget binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 -yq
+# We download repo as zip file because it's faster than cloning it with git
+wget https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang/archive/refs/heads/ruby-12.zip
+unzip -qq ruby-12.zip
+
+# Build
+make O=out ARCH=arm64 pine-perf_defconfig
+PATH="$(pwd)/proprietary_vendor_qcom_sdclang-ruby-12/bin:${PATH}"
+make -j$(nproc --all) O=out ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip \
+
+# Build flashable zip
+cp out/arch/arm64/boot/dtbo.img AnyKernel3/
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3/
+zipfile="./out/NVKernel_$(date +%Y%m%d-%H%M).zip"
+7z a -mm=Deflate -mfb=258 -mpass=15 -r $zipfile ./AnyKernel3/*
+
+# Send flashable zip to Telegram channel
+escape() {
+    echo $1 | sed -Ee "s/([^a-zA-Z\s0-9])/\\\\\1/g"
+}
+
+FILE_CAPTION=$(cat << EOL
+*Branch:* $(escape $DRONE_BRANCH)
+*Commit:* [$(echo $DRONE_COMMIT | cut -c -7)]($(escape $DRONE_COMMIT_LINK))
+EOL
+)
+curl -F "document=@${zipfile}" --form-string "caption=${FILE_CAPTION}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=MarkdownV2"
